b83095edbc701fcbda437fbff164b265276285c1
Daniel Pink <dr.pink@gmail.com>
2015-01-05 22:48:28 +1000

    Fixed the typo I introduced and wrote out the answer to the question

--- Racket/Ch1-BuildingAbstractionsWithProcedures.rkt
+++ Racket/Ch1-BuildingAbstractionsWithProcedures.rkt
@@ -1,23 +1,23 @@
 #lang racket
 
-; SICP Excersize 1.1
-10
-(+ 5 3 4)
-(- 9 1)
-(/ 6 2)
-(+ (* 2 4) (- 4 6))
-(define a 3)
+; SICP Exercise 1.1
+10                               ; 10
+(+ 5 3 4)                        ; 12
+(- 9 1)                          ;  8
+(/ 6 2)                          ;  3
+(+ (* 2 4) (- 4 6))              ;  6
+(define a 3)                     
 (define b (+ a 1))
-(+ a b (* a b))
-(= a b)
+(+ a b (* a b))                  ; 19
+(= a b)                          ; #f
 (if (and (> b a) (< b (* a b)))
     b
-    a)
-(cond ((= a 4) 6)
+    a)                           ;  4 (b)
+(cond ((= a 4) 6)             
       ((= b 4) (+ 6 7 a))
-      (else 25))
-(+ 2 (if (> b a) b a))
+      (else 25))                 ; 16 (= b 4)
+(+ 2 (if (> b a) b a))           ;  6
 (* (cond ((> a b) a)
          ((< a b) b)
          (else -1))
-   (+ a 1))
+   (+ a 1))                      ; 16
