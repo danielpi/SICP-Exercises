aa73d40217abb4d38490b11c0e1de8afe210e706
Daniel Pink <dr.pink@gmail.com>
2015-02-02 22:45:37 +1000

    Split exercises 1.22 and 1.23 out into their own playgrounds so that their code is cleaner and the performance issues don’t hold everything else up.

---
+++ Swift/Ex1.22-SearchForPrimes.playground/section-1.swift
@@ -0,0 +1,123 @@
+import Cocoa
+
+// Exercise 1.22
+func isEven(n: Int) -> Bool {
+    return (n % 2) == 0
+}
+func square(x: Int) -> Int {
+    return x * x
+}
+func dividesWithNoRemainder(a: Int, b: Int) -> Bool {
+    return  a % b == 0
+}
+dividesWithNoRemainder(10, 2)
+
+func findDivisor(n: Int, testDivisor: Int) -> Int {
+    switch true {
+    case square(testDivisor) > n:
+        return n
+    case dividesWithNoRemainder(n, testDivisor):
+        return testDivisor
+    default:
+        return findDivisor(n, testDivisor + 1)
+    }
+}
+
+func smallestDivisor(n: Int) -> Int {
+    return findDivisor(n, 2)
+}
+
+func isPrime(n:Int) -> Bool {
+    return n == smallestDivisor(n)
+}
+
+func timedPrimeTest(n: Int) {
+    startPrimeTest(n, NSDate())
+}
+func startPrimeTest(n: Int, startTime: NSDate) {
+    if isPrime(n) {
+        reportPrime(n, -1 * startTime.timeIntervalSinceNow)
+    }
+}
+func reportPrime(n: Int, elapsedTime: Double) {
+    print("\n")
+    print("\(n)")
+    print(" *** ")
+    print("\(elapsedTime)")
+}
+timedPrimeTest(7013)
+
+// Using this procedure write a searchForPrimes that checks the primality of consecutive odd integers in a specified range. Use your procedure to find the three smallest primes larger than 1000, larger than 10000, larger than 100000, larger than 1000000.
+
+func searchForPrimes(a: Int, b: Int) {
+    switch true {
+    case a > b:
+        println("Complete")
+    case isEven(a):
+        searchForPrimes(a + 1, b)
+    default:
+        timedPrimeTest(a)
+        searchForPrimes(a + 2, b)
+    }
+}
+
+// Use your procedure to find the three smallest primes larger than
+// - 1000
+// - 10000
+// - 100000
+// - 1000000
+
+searchForPrimes(1000, 1030)
+// 1009 *** 0.23639303445816
+// 1013 *** 0.227622985839844
+// 1019 *** 0.216214001178741
+(0.23639303445816 + 0.227622985839844 + 0.216214001178741) / 3
+// Average time 0.00398
+
+//searchForPrimes(10000, 10050)
+// 10007 *** 0.727922022342682
+// 10009 *** 0.748418986797333
+// 10037 *** 0.735283017158508
+(0.727922022342682 + 0.748418986797333 + 0.735283017158508) / 3
+// Average time 0.012369
+
+//searchForPrimes(100000, 100050)
+// 100003 *** 2.24567699432373
+// 100019 *** 2.0843819975853
+// 100043 *** 2.03399205207825
+(2.24567699432373 + 2.0843819975853 + 2.03399205207825) / 3
+// Average time 0.03857
+
+//searchForPrimes(1000000, 1000050)
+// 1000003 *** 6.36476397514343
+// 1000033 *** 6.54135400056839
+// 1000037 *** 6.34981900453568
+(6.36476397514343 + 6.54135400056839 + 6.34981900453568) / 3
+// Average time 0.109049
+
+// Note the time needed to test each prime. Since the testing algorithm has an order of growth of O(n^0.5) you should expect primes around 10,000 to take about 10^0.5 times as long to test for as for primes around 1000.
+
+/*
+Times in DrRacket
+;         | Actual Times |    1000 |   10000 |  100000 | 1000000 |
+;    1000 |    0.00398   | 0.00398 | 0.00390 | 0.00385 | 0.00344 |
+;   10000 |    0.01236   | 0.01258 | 0.01236 | 0.01219 | 0.01090 |
+;  100000 |    0.03857   | 0.0398  | 0.03908 | 0.03857 | 0.03448 |
+; 1000000 |    0.10905   | 0.12585 | 0.1236  | 0.12196 | 0.10905 |
+
+Times in Swift
+;         | Actual Times |    1000 |   10000 |  100000 | 1000000 |
+;    1000 |    0.22674   | 0.22674 | 0.23312 | 0.21213 | 0.20297 |
+;   10000 |    0.73720   | 0.71702 | 0.73720 | 0.67082 | 0.64186 |
+;  100000 |    2.12135   | 2.26743 | 2.33125 | 2.12135 | 2.02975 |
+; 1000000 |    6.41864   | 7.17025 | 7.37208 | 6.70829 | 6.41864 |
+*/
+
+// Do your timing data bear this out?
+// Yes
+
+// How well do the data for 100,000 and 1,000,000 support the n prediction?
+// Pretty well, calculation seems to run better with a larger n
+
+// Is your result compatible with the notion that programs on your machine run in time proportional to the number of steps required for the computation?
+// Yep
---
+++ Swift/Ex1.23-SearchForPrimes.playground/section-1.swift
@@ -0,0 +1,105 @@
+
+import Cocoa
+
+// Exercise 1.23
+// The smallestDivisor procedure shown at the start of this section does lots of needless testing. After it checks to see if the number is divisible by 2 there is no point in checking to see if it is divisible by any larger even numbers. Define a procedure next that returns 3 if its input is equal to 2 and otherwise returns its input + 2
+
+func square(x: Int) -> Int {
+    return x * x
+}
+func dividesWithNoRemainder(a: Int, b: Int) -> Bool {
+    return  a % b == 0
+}
+func isEven(x:Int) -> Bool {
+    return dividesWithNoRemainder(x, 2)
+}
+func findDivisor(n: Int, testDivisor: Int) -> Int {
+    switch true {
+    case square(testDivisor) > n:
+        return n
+    case dividesWithNoRemainder(n, testDivisor):
+        return testDivisor
+    default:
+        return findDivisor(n, next(testDivisor))
+    }
+}
+
+func smallestDivisor(n: Int) -> Int {
+    return findDivisor(n, 2)
+}
+
+func isPrime(n:Int) -> Bool {
+    return n == smallestDivisor(n)
+}
+
+func timedPrimeTest(n: Int) {
+    startPrimeTest(n, NSDate())
+}
+func startPrimeTest(n: Int, startTime: NSDate) {
+    if isPrime(n) {
+        reportPrime(n, -1 * startTime.timeIntervalSinceNow)
+    }
+}
+func reportPrime(n: Int, elapsedTime: Double) {
+    print("\n")
+    print("\(n)")
+    print(" *** ")
+    print("\(elapsedTime)")
+}
+
+func searchForPrimes(a: Int, b: Int) {
+    switch true {
+    case a > b:
+        println("Complete")
+    case isEven(a):
+        searchForPrimes(a + 1, b)
+    default:
+        timedPrimeTest(a)
+        searchForPrimes(a + 2, b)
+    }
+}
+
+func next(n: Int) -> Int {
+    if n == 2 {
+        return 3
+    } else {
+        return n + 2
+    }
+}
+
+searchForPrimes(1000, 1030)
+//searchForPrimes(10000, 10050)
+//searchForPrimes(100000, 100050)
+//searchForPrimes(1000000, 1000050)
+
+
+/*
+Exercise 1.22 ran in the following time
+1009 *** 0.189490020275116
+1013 *** 0.199604034423828
+1019 *** 0.215083003044128
+1021 *** 0.211998045444489
+
+Exercise 1.23 ran in the following time
+1009 *** 0.187184989452362
+1013 *** 0.294092953205109
+1019 *** 0.20736700296402
+1021 *** 0.197423994541168
+
+
+Exercise 1.22 ran in the following time
+1000003 *** 7.16602700948715
+1000033 *** 8.14103001356125
+1000037 *** 7.98269999027252
+1000039 *** 7.72472196817398
+
+Exercise 1.23 ran in the following time
+1000003 *** 6.5731880068779
+1000033 *** 5.61217302083969
+1000037 *** 5.32903099060059
+1000039 *** 5.41905397176743
+
+Looks like they both ran in about the same amount of time for the lower primes. Its 2015-02-02 and Swift playground performance is a bit screwy
+For the higher primes we get a ration of about 1.4 rather than 2. Possibly this is due to the if statement in next() (and screwy Swift performance)
+*/
+ 7.72472196817398 / 5.41905397176743
