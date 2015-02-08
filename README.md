84b689d78701a6849a8c9f5d2c0fb1350ee79f9e
Daniel Pink <dr.pink@gmail.com>
2015-02-08 21:40:39 +1000

    Exploring Carmichael numbers in Swift.

--- README.md
+++ README.md
@@ -29,3 +29,7 @@ For most of the algorithms mentioned above the performance implications of multi
 Very enlightening so far.
 
 I have been using [DrRacket](http://racket-lang.org) as my lisp environment. I have seen it recommended that [MIT Scheme](http://www.gnu.org/software/mit-scheme/) is a better choice. For now I'll be sticking with DrRacket and to date I haven't run into any issues. There is a [SICP compatibility mode for DrRacket](http://www.neilvandyke.org/racket-sicp/), I haven't tried it out yet though it is apparently required for the later chapters.
+
+Using Swift to carry out the same exercises has been interesting. Racket makes Swift feel verbose. When I started this project I found Lisp incredibly hard to read. I really couldn't make head nor tail of it. Now that I am more familiar with I find that I like its terseness. This might all be due to the fact that I am working on concise, short-lived puzzles so far. 
+
+Swift playgrounds are very useful for this sort of experimentation. It is far easier than any other option that XCode provides. Performance is an issue though. Swift Playgrounds are much, much slower than DrRacket. To the point of not being useable for some algorithms. The execution count in the playground gutter to show how often a line of code has been called is very useful for exploring the growth of calculations.
---
+++ Swift/Ex1.27-Carmichael-Numbers.playground/section-1.swift
@@ -0,0 +1,78 @@
+import Darwin
+
+// Exercise 1.27
+// Demonstrate that Carmichael numbers really do fool the Fermat test. Write a procedure that takes an integer n and tests whether a^n is congruent to a modulo n for every a < n.
+
+
+func isEven(n: Int) -> Bool {
+    return (n % 2) == 0
+}
+func square(x: Int) -> Int {
+    return x * x
+}
+func dividesWithNoRemainder(a: Int, b: Int) -> Bool {
+    return  a % b == 0
+}
+
+func next(n: Int) -> Int {
+    if n == 2 {
+        return 3
+    } else {
+        return n + 2
+    }
+}
+
+func findDivisor(n: Int, testDivisor: Int) -> Int {
+    switch true {
+    case square(testDivisor) > n:
+        return n
+    case dividesWithNoRemainder(n, testDivisor):
+        return testDivisor
+    default:
+        return findDivisor(n, next(testDivisor))
+    }
+}
+
+func smallestDivisor(n: Int) -> Int {
+    return findDivisor(n, 2)
+}
+
+func isPrime(n:Int) -> Bool {
+    return n == smallestDivisor(n)
+}
+
+
+func expMod(base: Int, exp: Int, m: Int) -> Int {
+    switch true {
+    case exp == 0:
+        return 1
+    case isEven(exp):
+        return square(expMod(base, exp / 2, m)) % m
+    default:
+        return (base * expMod(base, exp - 1, m)) % m
+    }
+}
+
+func carmichaelTestIter(n: Int, a: Int) -> Bool {
+    switch true {
+    case a == 0:
+        return true
+    case expMod(a, n, n) == a:
+        return carmichaelTestIter(n, a - 1)
+    default:
+        return false
+    }
+}
+
+func isCarmichaelNumber(n: Int) -> Bool {
+    if isPrime(n) {
+        return false
+    } else {
+        return carmichaelTestIter(n, n - 1)
+    }
+}
+
+//isCarmichaelNumber(7)
+isCarmichaelNumber(561)
+
+
