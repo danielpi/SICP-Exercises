17f5646323a31903809b447d7dd8ae81e62e68ce
Daniel Pink <dr.pink@gmail.com>
2015-02-14 22:33:02 +1000

    Higher-order functions in Swift

--- Swift/1.3-FormulatingAbstractionsWithHigher-Order-Procedures.playground/section-1.swift
+++ Swift/1.3-FormulatingAbstractionsWithHigher-Order-Procedures.playground/section-1.swift
@@ -53,4 +53,44 @@ func <name>(a: Int, b: Int) -> ??? {
 }
 */
 
+// The pattern described above is very similar to the mathematical concept of summation. This allows mathematicians to deal with the concept of summation rather than simply a particular instance of summation.
 
+func sum(term: (Int) -> Double, a: Int, next: (Int) -> Int, b: Int) -> Double {
+    if a > b {
+        return 0
+    } else {
+        return term(a) + sum(term, next(a), next, b)
+    }
+}
+
+// We can recreate the procedures from above using our sum() function.
+
+func inc(n: Int) -> Int {
+    return n + 1
+}
+func cubeDouble(x: Int) -> Double {
+    return Double(x * x * x)
+}
+func sumCubes2(a: Int, b: Int) -> Double {
+    return sum(cubeDouble, a, inc, b)
+}
+sumCubes2(1, 10)
+
+func identity(x:Int) -> Double {
+    return Double(x)
+}
+func sumIntegers2(a: Int, b: Int) -> Double {
+    return sum(identity, a, inc, b)
+}
+sumIntegers2(1, 10)
+
+func piSum2(a:Int, b:Int) -> Double {
+    func piTerm(x: Int) -> Double {
+        return 1.0 / (Double(x) * (Double(x) + 2.0))
+    }
+    func piNext(x: Int) -> Int {
+        return x + 4
+    }
+    return sum(piTerm, a, piNext, b)
+}
+8 * piSum2(1, 1000)
