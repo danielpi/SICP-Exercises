681cee703c9e42242b1cf87d6fe90e58e7f48c71
Daniel Pink <dr.pink@gmail.com>
2015-02-18 23:54:47 +1000

    Exercise 1.31

---
+++ Swift/Ex1.31-Product.playground/section-1.swift
@@ -0,0 +1,76 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+// Exercise 1.31
+// a) The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures. Write an analogous procedure called product that returns the product of the values of a function at points over a given range. 
+
+protocol MultipliableType: IntegerLiteralConvertible {
+    func *(lhs: Self, rhs: Self) -> Self
+}
+extension Double : MultipliableType {}
+extension Float  : MultipliableType {}
+extension Int    : MultipliableType {}
+
+protocol AddableType: IntegerLiteralConvertible {
+    func +(lhs: Self, rhs: Self) -> Self
+}
+extension Double : AddableType {}
+extension Float  : AddableType {}
+extension Int    : AddableType {}
+
+
+
+func cube<T:MultipliableType>(x: T) -> T {
+    return x * x * x
+}
+func identity<T>(x: T) -> T {
+    return x
+}
+func inc(n: Int) -> Int {
+    return n + 1
+}
+func isEven(n: Int) -> Bool {
+    return (n % 2) == 0
+}
+
+
+func product<T:Comparable,U:MultipliableType>(term:(T) -> U, a:T, next:(T) -> T, b:T) -> U {
+    if a > b {
+        return 1
+    } else {
+        return term(a) * product(term, next(a), next, b)
+    }
+}
+
+// Show how to define factorial in terms of product.
+
+func factorial(n: Int) -> Int {
+    return product(identity, 1, inc, n)
+}
+factorial(5)
+
+// Use product to compute approximations to pi using the formula
+/*
+
+pi   2 * 4 * 4 * 6 * 6 * 8 ...
+-- = -------------------------
+ 4   3 * 3 * 5 * 5 * 7 * 7 ...
+
+*/
+
+func DRPpi(steps: Int) -> Double {
+    func term(k: Int) -> Double {
+        if isEven(k) {
+            return (k + 2) / (k + 1)
+        } else {
+            
+        }
+    }
+}
+
+
+
+
+
+
