b70039b861fe38694afc578b31c1796b2f07b922
Daniel Pink <dr.pink@gmail.com>
2015-02-20 19:30:35 +1000

    Working through the accumulate section in Swift

--- Swift/Ex1.32-Accumulate.playground/section-1.swift
+++ Swift/Ex1.32-Accumulate.playground/section-1.swift
@@ -3,4 +3,85 @@ import Cocoa
 // Exercise 1.32
 // a) Show that sum and product are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general function
 
-// func accumulate(combiner:
+// func accumulate(combiner:,nullValue:, term: ,a:, next: , b:) ->
+
+// Write accumulate and show how sum and product can both be defined as simple calls to accumulate
+func accumulateDouble(combiner: (Double, Double) -> Double,
+                     nullValue: Double,
+                          term: (Int) -> Double,
+                             a: Int,
+                          next: (Int) -> Int,
+                             b: Int) -> Double {
+        if (a > b) {
+            return nullValue
+        } else {
+            return combiner(term(a), accumulateDouble(combiner, nullValue, term, next(a), next, b))
+        }
+}
+
+// Implementing sum
+func sumDouble(term:(Int) -> Double, a: Int, next: (Int) -> Int, b: Int) -> Double {
+    return accumulateDouble(+, 0.0, term, a, next, b)
+}
+
+
+func inc(n: Int) -> Int {
+    return n + 1
+}
+func isEven(n: Int) -> Bool {
+    return (n % 2) == 0
+}
+
+func integral(f:(Double) -> Double, a:Double, b:Double, n:Int) -> Double {
+    func h(a: Double, b: Double, n: Int) -> Double {
+        return (b - a) / Double(n)
+    }
+    func yk(f:(Double) -> Double, a: Double, b: Double, n:Int, k:Int) -> Double {
+        let x = a + (Double(k) * h(a,b,n))
+        return f(x)
+    }
+    func simpsonsTerm(k:Int) -> Double {
+        switch true {
+        case k == 0 || k == n:
+            return yk(f, a, b, n, k)
+        case isEven(k):
+            return 2 * yk(f, a, b, n, k)
+        default:
+            return 4 * yk(f, a, b, n, k)
+        }
+    }
+    return (h(a,b,n) / 3.0) * sumDouble(simpsonsTerm, 0, inc, n)
+}
+
+func cube(x: Double) -> Double {
+    return x * x * x
+}
+func identity<T>(x: T) -> T {
+    return x
+}
+integral(cube, 0, 1, 100)
+integral(identity, 0, 1, 100)
+
+
+// Implementing Product
+func productDouble(term:(Int) -> Double, a: Int, next: (Int) -> Int, b: Int) -> Double {
+    return accumulateDouble(*, 1.0, term, a, next, b)
+}
+
+
+func DRPpi(steps: Int) -> Double {
+    func term(k: Int) -> Double {
+        if isEven(k) {
+            return (Double(k) + 2) / (Double(k) + 1)
+        } else {
+            return (Double(k) + 1) / (Double(k) + 2)
+        }
+    }
+    return 4 * productDouble(term, 1, inc, steps)
+}
+
+DRPpi(100)
+
+
+
+// b)
