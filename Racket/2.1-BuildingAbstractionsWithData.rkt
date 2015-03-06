7599787be4792059e3503720074e382203578415
Daniel Pink <dr.pink@gmail.com>
2015-03-06 22:20:54 +1000

    Starting on Chapter 2

---
+++ Racket/2.1-BuildingAbstractionsWithData.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+; Chapter 2 - Building Abstractions with Data
+
+; Chapter 1 was focused on computational processes and the role of procedures in program design. We saw how
+; to use primitive data (numbers) and primitive operations (arithmetic operations), how to combine procedures 
+; to form compond procedures through composition, continionals adn the use of parameters.
+
+; In chapter two we are going to look at more complex data. Chapter 1 was about building abstractions by
+; combining procedures to form compound procedures, we turn in this chapter to another key aspect of any
+; programming language: the means it provides for building abstractions by combining data objects to form
+; combound data
+
+; Why do we want combound data in a programming language?
+; - to elevate the conceptual level at which we can design our programs
+; - to increase the modularity of our designs
+; - enhance expressive power
+
+
+; Section 2.1
+; Introduction to Data Abstraction
+; 
