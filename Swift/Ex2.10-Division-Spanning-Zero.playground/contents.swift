963178c1815a737fecad611b1a415d79e3d9a672
Daniel Pink <dr.pink@gmail.com>
2015-04-01 22:50:00 +1000

    Started on Ex 2.10 in Swift but XCode kept crashing.

---
+++ Swift/Ex2.10-Division-Spanning-Zero.playground/contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// Exercise 2.10
+// Ben Bitdiddle, an expert systems programmer, looks over Alyssa's shoulder and comments that it is not clear what it means to divide by an interval that spans zero. Modify Alyssa's code to check for this condition and to signal an error if it occurs.
