016c68bb545cb9026f39145d8526c1b2c4a9accd
Daniel Pink <dr.pink@gmail.com>
2015-04-13 11:44:17 +1000

    Exercise 2.23 in Swift and Racket

---
+++ Swift/Ex2.23-For-Each.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+func cons<A>(value: A, list: [A]) -> [A] {
+    var newList = list
+    newList.insert(value, atIndex: 0)
+    return newList
+}
+func car<A>(list:[A]) -> A {
+    return list[0]
+}
+func cdr<A>(list:[A]) -> [A] {
+    return Array(list[1..<list.count])
+}
+func map<T, U>(proc:(T) -> U, items: [T]) -> [U] {
+    if items.isEmpty {
+        return []
+    } else {
+        return cons(proc(car(items)), map(proc, cdr(items)))
+    }
+}
+func square(x: Int) -> Int { return x * x }
+
+// Exercise 2.23
+// The procedure for-each is similar to map. It takes as arguments a procedure and a list of elements. However, rather than forming a list of the results, for-each just applies the procedure to each of the elements in turn, from left to right. The values returned by applying the procedure to the elements are not used at all -- for-each is used with proedures that perform an action, such as printing. For example,
+
+for x in [57, 321, 88] {
+    println("\(x)")
+}
+
+// The value returned by the call to for-each (not illustrated above) can be something arbitrary, such as true. Give an implmentation of for-each
+
+func forEach<T>(proc: (T) -> (), items: [T]) {
+    if !items.isEmpty {
+        proc(car(items))
+        forEach(proc, cdr(items))
+    }
+}
+
+forEach({ x in println("\(x)") }, [57, 321, 88])
+
+
