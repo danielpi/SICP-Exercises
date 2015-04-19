cb01cf5f621132b7b48b925192a5df5ca51ee139
Daniel Pink <dr.pink@gmail.com>
2015-04-19 23:11:26 +1000

    Some slight progress with Swift. Having difficulty with lists in Swift.

--- Swift/Ex2.24-Tree-Diagrams.playground/Contents.swift
+++ Swift/Ex2.24-Tree-Diagrams.playground/Contents.swift
@@ -3,3 +3,8 @@ import Cocoa
 // Exercise 2.24
 // Suppose we evaluate the expression (list 1 (list 2 (list 3 4))). Give the result printed by the interpreter, the corresponding box-and-pointer structure, and the interpretation of this as a tree
 
+//List(1, List(2), List(arrayLiteral: 3,4))
+List(1)
+List(2)
+List(3,4)
+
---
+++ Swift/Ex2.25-Moving-Through-Trees.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+// Exercise 2.25
+// Give combinations of cars of cdrs that will pick 7 from each of the following lists:
+
+
+
