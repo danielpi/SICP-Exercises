0126b95bc9a12ba08b68dc934eb661cee8db5641
Daniel Pink <dr.pink@gmail.com>
2015-04-26 23:05:58 +1000

    Slow progress on this one

--- Racket/Exercise2-32-Subsets-of-Set.rkt
+++ Racket/Exercise2-32-Subsets-of-Set.rkt
@@ -9,7 +9,50 @@
 
 (define (subsets s)
   (if (null? s)
-      (list nil)
+      (list '())
       (let ((rest (subsets (cdr s))))
-        (append rest (map (??) rest)))))
+        (append rest (map (lambda (subset) s) rest)))))
 
+(subsets (list 1 2 3))
+
+; (subset (1 2 3))
+; (append (subsets (2 3)) (map (??) (subsets (2 3))))))
+; (append ((append (subsets (3)) (map (??) (subsets (3))))))) (map (??) (append (subsets (3)) (map (??) (subsets (3))))))))))
+; (append ((append (append (subsets ()) (map (??) (subsets ()))))) (map (??) (append (subsets ()) (map (??) (subsets ())))))))))) (map (??) (append (append (subsets ()) (map (??) (subsets ()))))) (map (??) (append (subsets ()) (map (??) (subsets ())))))))))))))
+; (append ((append (append () (map (??) ())))) (map (??) (append () (map (??) ()))))))))) (map (??) (append (append () (map (??) ())))) (map (??) (append () (map (??) ()))))))))))))
+; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
+
+
+(subsets (list 2 3))
+
+; (subset (2 3))
+; (s (2 3)) (rest (subset (3)) (append rest (map (??) rest))
+; (s (2 3)) (append (() (3)) (map (??) (() (3))))
+;(lambda (the-rest) 
+;  (if (null? the-rest)
+;      s
+;      (exclude
+; (() (3) (2) (2 3))
+
+(subsets (list 3))
+; (s (3)) (rest (subsets '()) (append rest (map (??) rest))
+; (() (3))
+
+
+(subsets '())
+; (s '()) (list '()) 
+; (())
+(define (contains? A b)
+  (cond ((null? A) #f)
+        ((= (car A) b) #t)
+        (else (or (contains? (cdr A) b)))))
+(contains? (list 1 2 3) 1)
+(contains? (list 1 2 3) 3)
+(contains? (list 1 2 3) 5)
+
+(define (relative-complement A B)
+  (define (rc A b)
+    
+  )
+;(relative-complement (list 2 3) (list 3)) ; (2)
+;(relative-complement (list 1 2 3) (list 2)) ; (1 3)
