da64b61d3094ddde68bea27fff4d7974b6a4a3fa
Daniel Pink <dr.pink@gmail.com>
2015-06-03 08:55:03 +1000

    Exercise 2.39 in Swift

---
+++ Swift/Ex2.39-Reverse-with-fold.playground/Contents.swift
@@ -0,0 +1,51 @@
+import Cocoa
+
+// Exercise 2.39
+// Complete the following definitions of reverse in terms of fold-right and fold-left
+
+
+func cons<A>(value: A, list: [A]) -> [A] {
+    var newList = list
+    newList.insert(value, atIndex: 0)
+    return newList
+}
+func car<A>(list:[A]) -> A {
+    return list[0]
+}
+func cdr<A>(list:[A]) -> [A] {
+    return Array(list[1..<list.count])
+}
+
+
+func foldl<A,B>(op: (B, A) -> B, initial: B, sequence:[A]) -> B {
+    var iter: (B, [A]) -> B = { (a,_) in return a }
+    
+    iter = { (result, rest) in
+        if rest.isEmpty {
+            return result
+        } else {
+            return iter(op(result, car(rest)), cdr(rest))
+        }
+    }
+    return iter(initial, sequence)
+}
+
+func foldr<A,B>(op: (A, B) -> B, initial: B, sequence: [A]) -> B {
+    if sequence.isEmpty {
+        return initial
+    } else {
+        return op(car(sequence), foldr(op, initial, cdr(sequence)))
+    }
+}
+
+
+func reverse1(sequence: [Int]) -> [Int] {
+    return foldr({ (x, y) in y + [x] }, [], sequence)
+}
+reverse1([1,2,3])
+
+
+func reverse2(sequence: [Int]) -> [Int] {
+    return foldl({ (x, y) in [y] + x }, [], sequence)
+}
+reverse2([1,2,3])
