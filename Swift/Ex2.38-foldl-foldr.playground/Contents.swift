188e1d24c900323fb00331baf14776b65f164b41
Daniel Pink <dr.pink@gmail.com>
2015-06-02 22:08:40 +1000

    Completed exercise 2.38 in Swift and Racket

--- Swift/Ex2.38-foldl-foldr.playground/Contents.swift
+++ Swift/Ex2.38-foldl-foldr.playground/Contents.swift
@@ -16,8 +16,8 @@ func cdr<A>(list:[A]) -> [A] {
 }
 
 
-func foldl<A>(op: (A, A) -> A, initial: A, sequence:[A]) -> A {
-    var iter: (A, [A]) -> A = { (a,_) in return a }
+func foldl<A,B>(op: (B, A) -> B, initial: B, sequence:[A]) -> B {
+    var iter: (B, [A]) -> B = { (a,_) in return a }
     
     iter = { (result, rest) in
         if rest.isEmpty {
@@ -29,7 +29,7 @@ func foldl<A>(op: (A, A) -> A, initial: A, sequence:[A]) -> A {
     return iter(initial, sequence)
 }
 
-func foldr<A>(op: (A, A) -> A, initial: A, sequence: [A]) -> A {
+func foldr<A,B>(op: (A, B) -> B, initial: B, sequence: [A]) -> B {
     if sequence.isEmpty {
         return initial
     } else {
@@ -41,6 +41,59 @@ func foldr<A>(op: (A, A) -> A, initial: A, sequence: [A]) -> A {
 foldr(/, 64.0, [2,2,2])
 foldl(/, 1.0, [1,2,3])
 
-//foldr(+, [], [1,2,3])
+
+class Box<T>{
+    let unbox: T
+    init(_ value: T) {
+        self.unbox = value
+    }
+}
+
+enum Tree<T> {
+    case Leaf(Box<T>)
+    case Node([Box<Tree<T>>])
+    
+    var stringRepresentation: String {
+        switch self {
+        case let .Leaf(value):
+            return " \(value.unbox)"
+        case let .Node(values):
+            let strings = map(values) { $0.unbox.stringRepresentation }
+            return "\(strings)"
+        }
+    }
+    
+    static func leaf(value: T) -> Tree<T> {
+        return Tree.Leaf(Box(value))
+    }
+    static func node(leaves: Tree<T>...) -> Tree<T> {
+        let boxed = map(leaves) { Box($0) }
+        return Tree.Node(boxed)
+    }
+    static func list(values: T...) -> Tree<T> {
+        let boxedValues = map(values) { Box($0) }
+        let leaves = map(boxedValues) { Tree.Leaf($0) }
+        let boxed = map(leaves) { Box($0) }
+        return Tree.Node(boxed)
+    }
+    static func cons(left: T, right: Tree<T>) -> Tree<T> {
+        let l = Tree.leaf(left)
+        return Tree.node(l, right)
+    }
+    static func cons(left: Tree<T>, right: T) -> Tree<T> {
+        let r = Tree.leaf(right)
+        return Tree.node(left, r)
+    }
+}
 
 
+let a = foldr(Tree.cons, Tree.Node([]), [1,2,3])
+a.stringRepresentation
+
+let b = foldl(Tree.cons, Tree.Node([]), [1,2,3])
+b.stringRepresentation
+
+
+// Give a property that op should satisfy to guarantee that fold-right and fold-left will produce the same values for any sequence
+
+// op needs to obey the commutativity property. This means that the function is the same in one direction as it is in the other.
