eb4733b16b77d9beca882a2cc398e6325c7fb6b5
Daniel Pink <dr.pink@gmail.com>
2015-06-22 22:44:21 +1000

    Frames in Swift

--- Swift/2.2.4 Example- A Picture Language.playground/Contents.swift
+++ Swift/2.2.4 Example- A Picture Language.playground/Contents.swift
@@ -124,7 +124,20 @@ draw(squareLimit2(wave, 4))
 
 
 // Frames
+// Before we can show how to implement painters and their means of combination, we must first consider frames. A frame can be described by three vectors - an origin vector, and two edge vectors. The origin vector specifies the offset of the frame's origin from some absolute origin in the plane, and the edge vectors specify the offsets of the frame's corners from its origin. If the edges are perpendicular, the frame will be rectangluar. Otherwise the frame will be a more general parallelogram.
+// Figure 2.15 shows a frame and its associated vectors. In accordance with data abstraction, we need not be specific yet about how frames are represented, other than to say that there is a constructor Frame(), which takes three vectors and produces a frame, and three corresponding selectors origin-frame, edge1-frame and edge2-frame.
+// We will use coordinates in the unit square (0 <= x, y <= 1) to specify images. With each frame, we associate a frame coordinate map, which will be used to shift and scale images to fit the frame. The map transforms the unit square into the frame by mapping the vector v = (x, y) to the vector sum 
 
+//          Origin(Frame) + x * Edge1(Frame) + y * Edge2(Frame)
+
+// For example, (0, 0) is mapped to the origin of the frame, (1,1) to the vertex diagonally opposite the origin, and (0.5, 0.5) to the center of the frame. We can create a frame's coordinate map with the following procedure:
+
+
+func frameCoordMap2(frame: Frame) -> (Vector) -> Vector {
+    return { vector in
+        return frame.origin + ((vector.x * frame.edge1) + (vector.y * frame.edge2))
+    }
+}
 
 
 
