43917157a36746b06f10064198fa68c5bab29541
Daniel Pink <dr.pink@gmail.com>
2015-06-30 12:39:56 +1000

    Some of these exercises don't make much sense because Swift operators are infix by default. It probably makes more sense to go in revers and use a revers polish notation and do exercises 2.58 and 2.57 in reverse order too

---
+++ Swift/Ex2.57-Variadic-Parameters.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+//: # Exercise 2.57
+// Extend the differentiation program to handle sums and products of arbitrary numbers of (two or more) terms. Then the last example above could be expressed as
+// 
+//     deriv(* "x" "y" (+ "x" 3), "x")
+//
+// Hmmm, I had already jumped to infix notation so I don't know what to do for this problem. WIll jump straight to 2.58 as it implies that my existing solution doesn't really take care of operator precedence at the moment.
+
+
