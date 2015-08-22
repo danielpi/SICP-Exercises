d77f3fed325e2662a9fb9c2dcabe0e39c54c029b
Daniel Pink <dr.pink@gmail.com>
2015-08-22 23:49:25 +1000

    Starting on section 2.4 in Swift.

---
+++ Swift/2.4-Multiple-Representations-for-Abstract-Data.playground/Contents.swift
@@ -0,0 +1,4 @@
+import Cocoa
+
+//: ## 2.4 Multiple Representations for Abstract Data
+//: We have introduced data abstraction, a methodology for structuring systems in such a way that much of a program can be specified independent of the choices involved in implementing the data objects that the program manipulates.
