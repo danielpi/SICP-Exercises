8713073a5f0e39863a2f816d35f4f2de8ffcf28a
Daniel Pink <dr.pink@gmail.com>
2015-12-25 23:23:24 +1000

    Started writing out exercise 2.77 in Swift. Merry Christmas

--- Swift/Ex2.76-ComplexErrorType.playground/Contents.swift
+++ Swift/Ex2.76-ComplexErrorType.playground/Contents.swift
@@ -1,50 +1,8 @@
 import Foundation
-//: # Systems with Generic Operations
-//:
-//: In the previous section, we saw how to design systems in which data objects can be represented in more than one way. The key idea is to link the code that specifies the data operations to the several representations by means of generic interface procedures. Now we will see how to use this same idea not only to define operations that are generic over different representations but also to define operations that are generic over different kinds of arguments. We have already seen several different packages of arithmetic operations
-//: - the primitive arithmetic (+, -, *, /) built into our language
-//: - the rational-number arithmetic (add-rat, sub-rat, mul-rat, div-rat) of Section 2.1.1
-//: - and the complex-number arithmetic that we implemented in Section 2.4.3.
-//:
-//: We will now use data-directed techniques to construct a package of arithmetic operations that incorporates all the arithmetic packages we have already constructed.
-//:
-//: Figure 2.23 shows the structure of the system we shall build. Notice the abstraction barriers. From the perspective of someone using "numbers", there is a single procedure *add* that operates on whatever numbers are supplied. *add* is part of a generic interface that allows the separate ordinary-arithmetic, rational-arithmetic and complex-arithmetic packages to be accessed uniformly by programs that use numbers. Any individual arithmetic package (such as the complex package) may itself be accessed through generic procedures (such as add-complex) that combine packages designed for different representations (such as rectangular and polar). Moreover, the structure of the system is additive, so that one can design the individual arithmetic packages separately and combine them to produce a generic arithmetic system.
-//:
-//:
-//:                            Programs that use numbers
-//:                               +-----------------+
-//:     --------------------------| add sub mul div |----------------------
-//:                               +-----------------+
-//:                             Generic arithmetic package
-//:       +-----------------+   +-------------------------+      +-----+
-//:     __| add-rat sub-rat |___| add-complex sub-complex |______| + - |___
-//:       | mul-rat div-rat | | | mul-complex div-complex | |    | * / |
-//:       +-----------------+ | +-------------------------+ |    +-----+
-//:                           |     Complex arithmetic      |
-//:            Rational       |-----------------------------|   Ordinary
-//:           arithmetic      |   Rectangular |    Polar    |  arithmetic
-//:     ----------------------+-----------------------------+--------------
-//:                 List structure and primitive machine arithmetic
-//:
-//:  Figure 2.23: Generic arithmetic system
-//:
-//:
-//: ## 2.5.1 Generic Arithmetic Operations
-//:
-//: The task of designing generic arithmetic operations is analogous to that of designing the generic complex-number operations. We would like, for instance, to have a generic addition procedure *add* that acts like ordinary primitive addition + on ordinary numbers, like add-rat on rational numbers, and like add-complex on complex numbers. We can implement add, and the other generic arithmetic operations, by following the same strategy we used in Section 2.4.3 to implement the generic selectors for complex numbers. We will attach a type tag to each kind of number and cause the generic procedure to dispatch to an appropriate package according to the data type of its arguments. 
-//:
-//: The generic arithmetic procedures are defined as follows:
 
-/*
-func add(x: Double, y: Double) -> Double { return applyGeneric("add")(x,y) }
-func sub(x: Double, y: Double) -> Double { return applyGeneric("sub")(x,y) }
-func mul(x: Double, y: Double) -> Double { return applyGeneric("mul")(x,y) }
-func div(x: Double, y: Double) -> Double { return applyGeneric("div")(x,y) }
-*/
+//: ## Exercise 2.77
+//: Louis Reasoner tries to evaluate the expression (magnitude z) where z is the object shown in Figure 2.24. To his surprise, instead of the answer 5 he gets an error message from apply-generic, saying there is no method for the operation magnitude on the types (complex). He shows this interaction to Alyssa P. Hacker, who says "The problem is that the complex-number selectors were never defined for complex numbers, just for polar and rectangular numbers. All you have to do to make this work is add the following to the complex package:"
 
-//: We begin by installing a package for handling *ordinary* numbers, that is, the primitive numbers of our language. We will tag these with the symbol *scheme-number*. The arithmetic operations in this package are the primitive arithmetic procedures (so there is no need to define extra procedures to handle the untagged numbers). Since these operations each take two arguments, they are installed in the table keyed by the list (scheme-number scheme-number):
-
-//typealias Tagged = (String, Double)
 enum NumberType {
     case number, complex, rational, polar, rectangular
 }
@@ -61,9 +19,6 @@ struct Tagged<V>: CustomStringConvertible {
 func attachTag<V>(tag: NumberType, value: V) -> Tagged<V> {
     return Tagged(type: tag, value: value)
 }
-
-
-// http://stackoverflow.com/questions/24021950/how-do-i-put-different-types-in-a-dictionary-in-the-swift-language
 
 struct TypeKey: Equatable, Hashable {
     let types: [NumberType]
@@ -112,8 +67,6 @@ func installSwiftNumberPackage() {
     put("make", TypeKey(types: [.number, .number]), { x in tag(x) })
 }
 
-//: Users of the Scheme-number package will create (tagged) ordinary numbers by means of the procedure:
-
 func makeSchemeNumber(n: Double) -> Tagged<Double> {
     if let make = get("make", TypeKey(types: [.number, .number])) as? (Double) -> Tagged<Double> {
         return make(n)
@@ -121,15 +74,6 @@ func makeSchemeNumber(n: Double) -> Tagged<Double> {
         fatalError("makeSchemeNumber hasn't been implemented")
     }
 }
-
-installSwiftNumberPackage()
-let a = makeSchemeNumber(9.4)
-let b = makeSchemeNumber(12.8)
-
-
-//: Now that the framework of the generic arithmetic system is in place, we can readily include new kinds of numbers. Here is a package that performs rational arithmetic. Notice that, as a benefit of additivity, we can use without modification the rational-number code from Section 2.1.1 as the internal procedures in the package:
-
-// This doesn't work at the moment, but I am getting bogged down.
 
 struct Pair: CustomStringConvertible {
     let car: Any
@@ -204,13 +148,6 @@ func makeRationalNumber(n: Int, d: Int) -> Tagged<Pair> {
         fatalError("makeSchemeNumber hasn't been implemented")
     }
 }
-
-installRationalPackage()
-let c = makeRationalNumber(3, d: 4)
-let d = makeRationalNumber(2, d: 5)
-
-
-//: We can install a similar package to handle complex numbers, using the tag complex. In creating the package, we extract from the table the operations make-from-real-imag and make-from-mag-ang that were defined by the rectangular and polar packages. Additivity permits us to use, as the internal operations, the same add-complex, sub-complex, mul-complex, and div-complex procedures from Section 2.4.1.
 
 func square(x:Double) -> Double { return x * x }
 
@@ -334,8 +271,6 @@ func installComplexPackage() {
 }
 installComplexPackage()
 
-//: Programs outside the complex-number package can construct complex numbers either from real and imaginary parts or from magnitudes and angles. Notice how the underlying procedures, originally defined in the rectangular and polar packages, are exported to the complex package, and exported from there to the outside world.
-
 func makeComplexFromRealImag(x: Double, y: Double) -> Tagged<Pair> {
     if let make = get("makeFromRealImag", TypeKey(types: [.complex])) as? (Double, Double) -> Tagged<Pair> {
         return make(x, y)
@@ -352,28 +287,4 @@ func makeComplexFromMagAng(r: Double, A: Double) -> Tagged<Pair> {
     }
 }
 
-//: What we have here is a two-level tag system. A typical complex number, such as 3 + 4i in rectangular form, would be represented as shown in Figure 2.24.
-//:
-//: Complex, Rectangular, 3, 4
-//: Figure 2.24 Representation of 3 + 4i in rectangular form.
-//:
-//: The outer tag (complex) is used to direct the number to the complex pachage. Once within the complex package, the next tag (rectangular) is used to direct the number to the rectangular package. In a large and complicated system there might be many levels, each interfaced with the next by means of generic operations. As a data object is passed "downward," the outer tag that is used to direct it to the appopriate package is stripped off (by applying contents) and the next level of tag (if any) becomes visible to be used for further dispatching.
-//:
-//: In the above packages, we used add-rat, add-complex, and the other arithmetic procedures exactly as originally written. Once these definitions are internal to different installation procedures, however, they no longer need names that are distinct from each other: we could simply name them add, sub, mul, and div in both packages.
 
-func +<T> (lhs: Tagged<T>, rhs: Tagged<T>) -> Tagged<T> {
-    if let add = get("add", TypeKey(types: [lhs.type, rhs.type])) as? (T, T) -> Tagged<T> {
-        return add(lhs.value, rhs.value)
-    } else {
-        fatalError("Addition is not installed for types: \(TypeKey(types: [lhs.type, rhs.type])))")
-    }
-}
-
-(a + b).value
-
-
-//: ## 2.5.2 Combining Data of Different Types
-//:
-//: We have seen how to define a unified arithmetic system that encompasses ordinary numbers, complex numbers, rational numbers, and any other type of number we might decide to invent, but we have ignorde, an important issue. The operations we have defined so far treat the different data types as being competely independent. Thus, there are separate packages for adding, say, two ordinary numbers, or two complex numbers. What we have not yet considered is the fact that it is meaningful to define operations that cross the type boundaries, such as the addition of a complex number to an ordinary number. We have gone to great pains to introduce barriers between parts of our programs so that they can be developed and understood separately. We would like to introduce the cross-type operations in some carefully controlled way, so that we can support them without seriously violating our module boundaries.
-//:
-//: One way to handle cross-type operations is to design a different procedure for each possible combination of types for which the operation is valid. For example. We could extend the complex-number package so that it provides a procedure for adding complex numbers to ordinary numbers and installs this in the table using the tag(complex scheme-number):
